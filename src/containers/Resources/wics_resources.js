0c3b3a952340ee14b72b515c6caa29e475b16e54
alexa2001 <amlagunas70@gmail.com>
2020-12-20 16:26:18 -0600

    fixed linter errors

--- src/containers/Resources/wics_resources.js
+++ src/containers/Resources/wics_resources.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Subtitle, Subheading, Description, Button } from './styles'
+import {
+  Subtitle, Subheading, Description, Button,
+} from './styles'
 import Mentorship from './csmentorship.png'
 
 const WicsResources = () => (
@@ -10,35 +12,40 @@ const WicsResources = () => (
     }}
     />
     <Subheading name="mentorships">Mentorship</Subheading>
-    <Description class = "flex flex-row">
+    <Description class="flex flex-row">
       <div>
-      Each year, WiCS organizes a mentorship program designed to match students who are
-      interested in CS with upperclassmen mentors. WiCS will sponsor an activity between
-      mentors and mentees each month. This is a low and flexible time commitment, where you
-      can get advice about classes, concentrations, internships, and build strong relationships
-      with upperclassmen students and others in the WiCS community. It's FREE FOOD,
-      FRIENDSHIP, AND ADVICE on your own schedule!
+        Each year, WiCS organizes a mentorship program designed to match students who are
+        interested in CS with upperclassmen mentors. WiCS will sponsor an activity between
+        mentors and mentees each month. This is a low and flexible time commitment, where you
+        can get advice about classes, concentrations, internships, and build strong relationships
+        with upperclassmen students and others in the WiCS community. It&apos;s FREE FOOD,
+        FRIENDSHIP, AND ADVICE on your own schedule!
 
-      This mentorship program is one of the largest on campus--over 200 women in tech participate
-      and have said that WiCS mentorship has been an incredibly beneficial experience.
-      <br/>
-      <Button>
-      <a style={{
-        color:"white", 
-        textDecoration: "none", 
-        fontSize:"1.5em"
-        }} target="wics mentorship"
-        href="https://www.harvardwics.com/mentorships">Read More and Sign Up</a>
-      </Button>
+        This mentorship program is one of the largest on campus--over 200 women in tech participate
+        and have said that WiCS mentorship has been an incredibly beneficial experience.
+        <br />
+        <Button>
+          <a
+            style={{
+              color: 'white',
+              textDecoration: 'none',
+              fontSize: '1.5em',
+            }}
+            target="wics mentorship"
+            href="https://www.harvardwics.com/mentorships"
+          >
+            Read More and Sign Up
+          </a>
+        </Button>
 
       </div>
       <div>
         <img
-              alt="mentorship"
-              src={Mentorship}
-              style={{
-                width: '30vw', height: 'undefined', aspectRatio: '3', marginLeft:'5vw', marginTop: '4vh', 
-              }}
+          alt="mentorship"
+          src={Mentorship}
+          style={{
+            width: '30vw', height: 'undefined', aspectRatio: '3', marginLeft: '5vw', marginTop: '4vh',
+          }}
         />
       </div>
     </Description>
